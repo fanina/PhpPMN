92434ab9beb18d632cd42f28305285d7a88ca3ef
Fanina safae <safae.fanina@gmail.com>
2021-01-11 00:50:02 +0100

    Modification class (Classe)

--- www/src/core/Classe.php
+++ www/src/core/Classe.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace Room\core;
 
 use Room\models\Student;
+use Room\models\Teacher;
 
 
 class Classe
 {
     public $student = [];
+    public $teacher;
 
-        public function __construct(Student ...$students)
+        public function __construct( Teacher $teacher , Student ...$students)
     {
+        $this->teacher = $teacher;
         $this->student = $students;
     }
 
-    //get result 
-    
     public function getCountStudent(): int
     {
         return sizeof($this->student);
